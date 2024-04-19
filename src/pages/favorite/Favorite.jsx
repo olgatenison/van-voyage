2d943b17f4d65669b40fcb0e2776b2fd1d938376
olgatenison <88651744+olgatenison@users.noreply.github.com>
2024-04-19 22:12:08 +0300

    add items to favorite page

--- src/pages/favorite/Favorite.jsx
+++ src/pages/favorite/Favorite.jsx
@@ -1,5 +1,20 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
 const Favorite = () => {
-  return <>favorite</>;
+  const favorites = useSelector(state => state.favorites);
+  return (
+    <>
+      <div>
+        <h2>Favorite</h2>
+        <ul>
+          {favorites.map(van => (
+            <li key={van.id}>{van.name}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Favorite;
